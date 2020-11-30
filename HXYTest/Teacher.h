f871a25f380ac39e7cc6facd6b211cb82f9bd920
chenyongxu <chenyongxu@chinamcloud.com>
2020-11-30 15:45:13 +0800

    添加teacher

---
+++ HXYTest/Teacher.h
@@ -0,0 +1,17 @@
+//
+//  Teacher.h
+//  HXYTest
+//
+//  Created by 谭翔宇 on 2020/11/30.
+//  Copyright © 2020 chenyx. All rights reserved.
+//
+
+#import "Person.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Teacher : Person
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ HXYTest/Teacher.m
@@ -0,0 +1,13 @@
+//
+//  Teacher.m
+//  HXYTest
+//
+//  Created by 谭翔宇 on 2020/11/30.
+//  Copyright © 2020 chenyx. All rights reserved.
+//
+
+#import "Teacher.h"
+
+@implementation Teacher
+
+@end
